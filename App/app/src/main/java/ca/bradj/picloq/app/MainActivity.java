9979f0eecc394a5ef0ff74bbc0b856753432ab5f
bradsk88 <hi@bradj.ca>
2014-08-05 00:07:19 -0600

    Initial implementation.
    App is just a random picture with a clock (UTC) below it.  The layout is already good to go, however.  All that remains is allowing the user to select their pictures and time zone.

---
+++ App/app/src/main/java/ca/bradj/picloq/app/MainActivity.java
@@ -0,0 +1,71 @@
+package ca.bradj.picloq.app;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextClock;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+
+        BitmapDrawable bmap = (BitmapDrawable) this.getResources().getDrawable(R.drawable.wallpaper);
+        float bmapWidth = bmap.getBitmap().getWidth();
+        float bmapHeight = bmap.getBitmap().getHeight();
+
+        float wRatio = width / bmapWidth;
+        float hRatio = height / bmapHeight;
+
+        float ratioMultiplier = wRatio;
+// Untested conditional though I expect this might work for landscape mode
+        if (hRatio < wRatio) {
+            ratioMultiplier = hRatio;
+        }
+
+        int newBmapWidth = (int) (bmapWidth * ratioMultiplier);
+        int newBmapHeight = (int) (bmapHeight * ratioMultiplier);
+
+        ImageView iView = (ImageView) findViewById(R.id.clockImage);
+        iView.setLayoutParams(new RelativeLayout.LayoutParams(newBmapWidth, newBmapHeight));
+
+        TextClock clock = (TextClock) findViewById(R.id.textClock);
+        clock.setTimeZone("UTC");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
